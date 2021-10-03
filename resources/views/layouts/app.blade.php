b0de32c71f7798ddfee46ed0e64dcab8feb8f5ca
Richard Komatz <r.komatz@gmail.com>
2021-10-02 22:03:25 -0400

    refactored asset includes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,28 +8,31 @@
     <meta name="author" content="Creative Tim">
     <title>{{ $pageName = "Dashboard" }} :: {{ env('APP_NAME') }}</title>
     <!-- Favicon -->
-    <link rel="icon" href="/assets/img/brand/favicon.png" type="image/png">
+    <link rel="icon" href="{{ asset('/assets/img/brand/favicon.png') }}" type="image/png">
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
     <!-- Icons -->
-    <link rel="stylesheet" href="/assets/vendor/nucleo/css/nucleo.css" type="text/css">
-    <link rel="stylesheet" href="/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+    <link rel="stylesheet" href="{{ asset('/assets/vendor/nucleo/css/nucleo.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" type="text/css">
     <!-- Page plugins -->
+    <link rel="stylesheet" href="{{ asset('/assets/vendor/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/assets/vendor/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/assets/vendor/datatables.net-select-bs4/css/select.bootstrap4.min.css') }}">
     <!-- Argon CSS -->
-    <link rel="stylesheet" href="/assets/css/argon.css?v=1.2.0" type="text/css">
+    <link rel="stylesheet" href="{{ asset('/assets/css/argon.css?v=1.2.0" type="text/css') }}">
 </head>
 
 <body>
 <!-- Sidenav -->
-<nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+<nav class="sidenav navbar navbar-vertical fixed-left navbar-expand-xs navbar-light bg-white" id="sidenav-main">
     <div class="scrollbar-inner">
 
         <!-- Brand -->
-        <div class="sidenav-header align-items-center" style="background: #f6f9fc">
-            <a class="navbar-brand" href="javascript:void(0)">
-                <img src="/assets/img/brand/SoldStack-small.svg" class="navbar-brand-img h-75" alt="...">
+        <div class="sidenav-header align-items-center mb-1" style="background: #f6f9fc;
+        background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(177,189,237,.3) 100%);">
+            <a href="javascript:void(0)">
+                <img src="{{ asset('/assets/img/brand/SoldStack-small.svg') }}" class="px-3 py-3" alt="...">
             </a>
-            <hr class="mt--1 opacity-1">
         </div>
 
         @include('layouts.navigation')
@@ -52,20 +55,27 @@
 
 <!-- Argon Scripts -->
 <!-- Core -->
-<script src="/assets/vendor/jquery/dist/jquery.min.js"></script>
-<script src="/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<script src="/assets/vendor/js-cookie/js.cookie.js"></script>
-<script src="/assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
-<script src="/assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+<script src="{{ asset('/assets/vendor/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('/assets/vendor/js-cookie/js.cookie.js') }}"></script>
+<script src="{{ asset('/assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
+<script src="{{ asset('/assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js') }}"></script>
 <!-- Datatables -->
-<link rel="stylesheet" href="/assets/vendor/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
-<link rel="stylesheet" href="/assets/vendor/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css">
-<link rel="stylesheet" href="/assets/vendor/datatables.net-select-bs4/css/select.bootstrap4.min.css">
+<script src="{{ asset('/assets/vendor/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('/assets/vendor/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('/assets/vendor/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('/assets/vendor/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('/assets/vendor/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('/assets/vendor/datatables.net-buttons/js/buttons.flash.min.js') }}"></script>
+<script src="{{ asset('/assets/vendor/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
+<script src="{{ asset('/assets/vendor/datatables.net-select/js/dataTables.select.min.js') }}"></script>
 <!-- Optional JS -->
-<script src="/assets/vendor/chart.js/dist/Chart.min.js"></script>
-<script src="/assets/vendor/chart.js/dist/Chart.extension.js"></script>
+<script src="{{ asset('/assets/vendor/chart.js/dist/Chart.min.js') }}"></script>
+<script src="{{ asset('/assets/vendor/chart.js/dist/Chart.extension.js') }}"></script>
 <!-- Argon JS -->
-<script src="/assets/js/argon.js?v=1.2.0"></script>
+<script src="{{ asset('/assets/js/argon.js?v=1.2.0') }}"></script>
+
+{{ $scripts ?? '' }}
 </body>
 
 </html>
